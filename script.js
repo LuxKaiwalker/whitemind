9584dea9f8a723ae3d03905639014167c4108a57
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-06-24 18:11:25 +0200

    added .js file + demo angular functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// Define a component
+@Component({
+    selector: 'app-root',
+    template: `
+        <h1>Hello world!</h1>
+        <p>{{ message }}</p>
+    `,
+})
+export class AppComponent {
+    message = 'This should display some messageS';
+}
+
+// Define a module
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+// Bootstrap the Angular app
+platformBrowserDynamic().bootstrapModule(AppModule);
