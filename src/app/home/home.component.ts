eb642fb0f56a84741b72957f9b0bacf7e2fd0f4a
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-07-05 15:01:15 +0200

    moved a.component html to home + liks bugfix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
