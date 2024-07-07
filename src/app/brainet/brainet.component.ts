8076524f1fc302975c4b4a5081fa6e6af747251e
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-07-07 13:31:10 +0200

    drag and drop demo init

--- src/app/brainet/brainet.component.ts
+++ src/app/brainet/brainet.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -7,7 +8,7 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-brainet',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DragDropModule],
   templateUrl: './brainet.component.html',
   styleUrl: './brainet.component.css'
 })
