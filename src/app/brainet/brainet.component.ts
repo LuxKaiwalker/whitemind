fbf00af0007bbe40870818991048c9fe6c2f0174
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-07-22 23:33:47 +0200

    basic interface tests + graph component init

--- src/app/brainet/brainet.component.ts
+++ src/app/brainet/brainet.component.ts
@@ -6,18 +6,20 @@ import { NgFor } from '@angular/common';
 
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { GraphComponent } from '../graph/graph.component';
 
 @Component({
   selector: 'app-brainet',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, DragDropModule, CommonModule, NgFor],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DragDropModule, CommonModule, NgFor, GraphComponent],
   templateUrl: './brainet.component.html',
   styleUrl: './brainet.component.css'
 })
 export class BrainetComponent {
   boxes: string [] = [];
-  addNewBox(){
-    const newBox = `item ${this.boxes.length + 1}`;
-    this.boxes.push(newBox);    
+  addNewBox(value: string){
+    const newBox = `item ${this.boxes.length + 1},${value}`;
+    this.boxes.push(newBox);
+
   }
 }
