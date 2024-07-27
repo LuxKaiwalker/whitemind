bd8599985e552aa7ee991f0eb8595fa5b4093266
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-07-27 17:13:35 +0200

    basic interface structure with cdkdragdrop and menu bar set

--- src/app/brainet/brainet.component.ts
+++ src/app/brainet/brainet.component.ts
@@ -1,5 +1,8 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -8,13 +11,17 @@ import { CanvasComponent } from './canvas/canvas.component';
 @Component({
   selector: 'app-brainet',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CanvasComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CanvasComponent, DragDropModule, NgFor, CommonModule],
   templateUrl: './brainet.component.html',
   styleUrl: './brainet.component.css'
 })
 
 export class BrainetComponent{
-  
+  boxes: string [] = [];
+  addNewBox(value: string){
+    const newBox = `item ${this.boxes.length + 1},${value}`;
+    this.boxes.push(newBox);  
+  }
 }
 
 
--- src/app/brainet/canvas/canvas.component.ts
+++ src/app/brainet/canvas/canvas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-canvas',
   standalone: true,
@@ -11,24 +13,11 @@ export class CanvasComponent implements OnInit {
   @ViewChild('canvas', { static: true }) myCanvas!: ElementRef;
   ngOnInit(){
     const canvas: HTMLCanvasElement = this.myCanvas.nativeElement;
-    const context = canvas.getContext('2d');
-    if (context) {
-      this.#drawRectangle(context);
-      this.#drawTriangle(context);
-    }
-  }
-  #drawRectangle(context: CanvasRenderingContext2D) {
-    context.fillStyle = 'red';
-    context.fillRect(10, 10, 100, 100);
-    context.clearRect(20, 20, 50, 50);
-  }
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const ctx = canvas.getContext('2d');
+    
 
-  #drawTriangle(context: CanvasRenderingContext2D) {
-    context.beginPath();
-    context.moveTo(150,70);
-    context.lineTo(200,20);
-    context.lineTo(200, 120);
-    context.lineTo(150,70);
-    context.fill();
+
   }
 }
