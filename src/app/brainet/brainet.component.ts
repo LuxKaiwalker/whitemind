af3d21ea85a8d4a888536d11fe8c5dff94a38ad1
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-07-28 21:13:46 +0200

    implement input forward pass to canvas but there were still bugs

--- src/app/brainet/brainet.component.ts
+++ src/app/brainet/brainet.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { DragDropModule, CdkDragEnd, CdkDragMove } from '@angular/cdk/drag-drop';
 import { NgFor } from '@angular/common';
 import { CommonModule } from '@angular/common';
 
@@ -20,6 +20,8 @@ export class BrainetComponent{
 
   //list of all boxes on screen or available
   boxes: string[][] = [];//dim 1: type of box; dim 2: num of box
+  message: string = '';
+  position: {x: number, y: number} = {x: 0, y: 0};
 
   addBox(typ: number){
     if(!this.boxes[typ]){//constructor for new box category if not initialized
@@ -27,8 +29,27 @@ export class BrainetComponent{
     }
     const newBox = `box nummer ${this.boxes[typ].length + 1}, typ:${typ}`;
     this.boxes[typ].push(newBox);
-    console.log(this.boxes);
   }
+
+  /**
+   * @brief function to be called when a drag event is detected
+   * @param $event 
+   * @note used to pass down component data to canvas
+   */
+  dragEnd($event: CdkDragEnd) {
+    console.log($event.source.getFreeDragPosition());
+
+    this.message = $event.source.element.nativeElement.innerText;
+    this.position = $event.source.getFreeDragPosition();
+  }
+
+ /**
+ * @brief function to be called when a drag event is detected
+ * @param $event 
+ * @note may be useful for drag and drop animations later
+ */
+  dragMoved($event: CdkDragMove) {
+    //console.log($event.source.getFreeDragPosition());
+  }
+
 }
-
-
--- src/app/brainet/canvas/canvas.component.ts
+++ src/app/brainet/canvas/canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 
 
 
@@ -11,20 +11,27 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class CanvasComponent implements OnInit {
   @ViewChild('canvas', { static: true }) myCanvas!: ElementRef;
+  @Input() message: string = '';
+  @Input() position: {x: number, y: number} = {x: 0, y: 0};
+  
   ngOnInit(){
     const canvas: HTMLCanvasElement = this.myCanvas.nativeElement;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     const ctx = canvas.getContext('2d');
-    
+
     if(ctx){
-
+      this.drawText(ctx, this.position.x, this.position.y, this.message);
     }
-
+  
   }
   drawRect(ctx: CanvasRenderingContext2D, x: number, y: number, width: number, height: number){
     ctx.beginPath();
     ctx.rect(x, y, width, height);
     ctx.stroke
+  }
+  drawText(ctx: CanvasRenderingContext2D, x: number, y: number, text: string){
+    ctx.font = '30px Arial';
+    ctx.fillText(text, x, y);
   }
 }
