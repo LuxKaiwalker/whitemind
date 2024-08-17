c3302c747861fd66bbedfc769222c5b048b5ad8f
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-08-17 19:42:39 +0200

    restoring commits cuz im dumb

--- src/app/brainet/canvas/brainet.canvas.ts
+++ src/app/brainet/canvas/brainet.canvas.ts
@@ -26,10 +26,10 @@ export class Canvas{
     drawArrow(startX: number, startY: number, endX: number, endY: number) {
 
         // Control points for the cubic bezier curve (can be adjusted for the desired curve shape)
-        const controlX1 = startX + (endX - startX) / 3;
-        const controlY1 = startY - 50;  // Adjust these values to change the curve
-        const controlX2 = endX - (endX - startX) / 3;
-        const controlY2 = endY - 50;  // Adjust these values to change the curve
+        const controlX1 = startX + Math.abs((endX - startX)/2);
+        const controlY1 = startY + Math.abs((endY - startY)/4);  // Adjust these values to change the curve
+        const controlX2 = endX - Math.abs((endX - startX)/2);
+        const controlY2 = endY - Math.abs((endY-startY)/4);  // Adjust these values to change the curve
     
         // Draw the cubic bezier curve
         this.ctx.beginPath();
