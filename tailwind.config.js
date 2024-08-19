b56faebaa7a768314bd785ae0af76ec59b0862f2
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-08-19 20:20:03 +0200

    tailwind migration part 3 + homepage structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         light_gray: "#f4f4f4",
         dark: "#333333",
       },
+      backgroundImage: {
+        "home-light": "url('/assets/pictures/home_background_light.jpg')",
+        "home-dark": "url('/assets/pictures/home_background.jpg')",
+      },
     },
   },
   plugins: [],
