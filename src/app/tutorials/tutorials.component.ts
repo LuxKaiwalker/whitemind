1d7cd53f2fdf6e0276ae45aaa04ac05715d8e329
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-08-24 15:01:46 +0200

    router link insert in tutorials +fine ajustments in header

--- src/app/tutorials/tutorials.component.ts
+++ src/app/tutorials/tutorials.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { TutorialMdComponent } from '../tutorial-md/tutorial-md.component';
+
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
  
 
 @Component({
   selector: 'app-tutorials',
   standalone: true,
-  imports: [RouterOutlet, TutorialMdComponent],
+  imports: [RouterLink, RouterOutlet, TutorialMdComponent, HeaderComponent, FooterComponent],
   templateUrl: './tutorials.component.html',
   styleUrl: './tutorials.component.css'
 })
 export class TutorialsComponent {
+assets: any;
   onLoad(event: any) {
     console.log(event);
   }
