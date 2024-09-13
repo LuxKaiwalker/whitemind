441e8729ce6cc2bf51ef6858bbef90b5db3f290e
Vincent Xigu Liu <vincent.xigu.liu@gmail.com>
2024-09-13 17:36:03 +0200

    handle more intuivtive now. still need rm function

--- src/app/brainet/draggables/brainet.handle.ts
+++ src/app/brainet/draggables/brainet.handle.ts
@@ -2,8 +2,10 @@ export class Handle {
     type: string;
     left: number;
     top: number;
-    height: number = 20;
-    width: number = 20;
+    height: number = 25;
+    width: number = 40;
+
+    connected: boolean;
 
     box_pos: {x: number, y: number} = {x: 0, y: 0};//the relative position to the box where the arrow should spawn
 
@@ -12,28 +14,36 @@ export class Handle {
     constructor(typ: string){
         this.type = typ.toString();
         if(this.type == "output"){//declares new handle
-            this.left = 135;
+            this.left = 120;
             this.top = 10;
             this.color = "#BB4430";//persian red
             this.box_pos.x = 160;
+
+            this.connected = true;
         }
         else if(this.type == "input"){
-            this.left = 5;
+            this.left = 0;
             this.top = 10;
             this.color = "#008080";//teal
             this.box_pos.x = 0;
+
+            this.connected = false;
         }
         else if(this.type == "special"){
-            this.left = 135;
-            this.top = 35;
+            this.left = 120;
+            this.top = 45;
             this.color = "#bad29f";//Celadon
             this.box_pos.x = 0;
+
+            this.connected = true;
         }
         else if(this.type == "config"){
-            this.left = 5;
-            this.top = 35;
+            this.left = 0;
+            this.top = 45;
             this.color = "#ff9914";//princeton orange
             this.box_pos.x = 0;
+
+            this.connected = true;
         }
         else{
             throw new Error(`Invalid handle type, given ${this.type}`);
